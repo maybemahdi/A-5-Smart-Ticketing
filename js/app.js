c08b252dbfb471502a1b960a92289adf04ae375e
maybemahdi <mh7266391@gmail.com>
2024-02-19 11:46:21 +0600

    Project-Ended

--- js/app.js
+++ js/app.js
@@ -144,7 +144,7 @@ for (let i = 0; i < seats.length; i++) {
               document.getElementById("discount-title").innerText = "Discount";
               let discountElement = document.getElementById("discount-price");
               let discount = (totalPrice * 20) / 100;
-              discountElement.innerText = "BDT" + discount.toFixed(2);
+              discountElement.innerText = "BDT " + discount.toFixed(2);
 
               //show grand total price
               let grandTotalPriceElement =
